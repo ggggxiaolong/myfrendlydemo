42db9faa2abf735615ca9254adae180a68c45343
mrtan <1120610800@qq.com>
2016-05-30 09:50:50 +0800

    添加ButterKnife测试mode

---
+++ butterknife/src/main/java/com/example/butterknife/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.butterknife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv)
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn)
+    void onClick(){
+        mTextView.setText("butterKnife is worked");
+    }
+}
